61e9143a66789019201466e192a2d454c287d73d
MarcelinaLukasik <98186787+MarcelinaLukasik@users.noreply.github.com>
2023-04-10 12:36:43 +0200

    Update GameManager.cs

--- Assets/Source/Core/GameManager.cs
+++ Assets/Source/Core/GameManager.cs
@@ -3,9 +3,6 @@ using UnityEngine;
 
 namespace DungeonCrawl.Core
 {
-    /// <summary>
-    ///     Loads the initial map and can be used for keeping some important game variables
-    /// </summary>
     public class GameManager : MonoBehaviour
     {
         private void Start()
